fb7b03cbbfb8489559b000b54eeafb99083ce79f
MattKellyplusplus <sendemailstomattkelly@gmail.com>
2015-03-26 02:13:19 +0000

    Items

---
+++ Item.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Item
+{
+	public string itemName;
+	public string itemID;
+	public string itemDescription;
+	public int itemMaxDurability;
+	public int itemCurDurability;
+	public int itemDamage;
+	public int itemSpeed;
+	public int itemEquipSlot;
+	public int itemValue;
+	public bool stackable;
+	public int itemStackSize;
+	public int itemMaxStack;
+	public Texture2D itemIcon;
+	public GameObject itemMesh;
+	public Texture3D itemTexture;
+	public ItemType itemType;
+	/*---Character Equipment Reference---
+	 * 0 - Head
+	 * 1 - Neck
+	 * 2 - Shoulder
+	 * 3 - Back
+	 * 4 - Hands
+	 * 5 - Left Hand
+	 * 6 - Right Hand
+	 * 7 - Torso
+	 * 8 - Legs
+	 * 9 - Feet
+	 */
+	
+	public enum ItemType
+	{
+		Weapon, 
+		Equipment,
+		Consumable,
+		Quest,
+		Trade,
+		Trash
+	}
+	public Item(string name, string ID, string desc, int maxDura, int curDura, int damage, int speed, int slot, int value, string type, bool stack)
+	{
+		itemName = name;
+		itemID = ID;
+		itemDescription = desc;
+		itemMaxDurability = maxDura;
+		itemCurDurability = curDura;
+		itemDamage = damage;
+		itemSpeed = speed;
+		itemEquipSlot = slot;
+		itemValue = value;
+		if(type == "Trade")
+		{
+			itemType = ItemType.Trade;
+			itemMaxStack = 40;
+		}
+		if(type == "Consumable")
+		{
+			itemType = ItemType.Consumable;
+			itemMaxStack = 40;
+		}
+		if(type == "Trash")
+		{
+			itemType = ItemType.Trash;
+			itemMaxStack = 20;
+		}
+		if(type == "Weapon")
+		{
+			itemType = ItemType.Weapon;
+		}
+		if(type == "Quest")
+		{
+			itemType = ItemType.Quest;
+		}
+		if(type == "Equipment")
+		{
+			itemType = ItemType.Equipment;
+		}
+		stackable = stack;
+		itemStackSize = 1;
+		itemMesh = Resources.Load<GameObject>("Models/" + itemName);
+		itemIcon = Resources.Load<Texture2D>("Icons/" + itemName);
+	}
+	public Item(string icon)
+	{
+		itemName = null;
+		itemIcon = Resources.Load<Texture2D>("Icons/" + icon);
+	}
+	public Item()
+	{
+		itemName = null;
+	}
+	public string getName() {
+		return itemName;
+	}
+	public void useConsumable()
+	{
+		this.itemStackSize--;
+	}
+}
---
+++ ItemDatabase.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour {
+	
+	public List<Item> items = new List<Item>();
+	//public Player player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+	
+	int itemID;
+	string stringID;
+	string endID;
+	/*---Character Equipment Reference---
+	 * 0 - Head
+	 * 1 - Neck
+	 * 2 - Shoulder
+	 * 3 - Back
+	 * 4 - Hands
+	 * 5 - Left Hand
+	 * 6 - Right Hand
+	 * 7 - Torso
+	 * 8 - Legs
+	 * 9 - Feet
+	 */
+	void Start () {
+		addItem ("Carrot", "Tasty Carrot", 0, 0, 15, 0, 0, 10, "Consumable", true);
+	}
+	public void addItem(string name, string desc, int maxDura, int curDura, int damage, int speed, int slot, int value, string type, bool stack)
+	{
+		if(itemID <=9)
+		{
+			stringID = "000";
+			endID = itemID.ToString();
+			stringID = string.Concat(stringID, endID);
+		}
+		else if(itemID > 9 && itemID < 100)
+		{
+			stringID = "00";
+			endID = itemID.ToString();
+			stringID = string.Concat(stringID, endID);
+		}
+		else if(itemID > 99 && itemID < 1000)
+		{
+			stringID = "0";
+			endID = itemID.ToString();
+			stringID = string.Concat(stringID, endID);
+		}
+		else if(itemID > 999 && itemID < 10000)
+		{
+			endID = itemID.ToString();
+			stringID = endID;
+		}
+		itemID++;
+		items.Add(new Item(name,stringID,desc,maxDura,curDura,damage,speed,slot, value, type, stack));
+	}
+	public Item returnItem(string id)
+	{
+		for (int i= 0; i<items.Count; i++)
+		{
+			if (id == items[i].itemID)
+			{
+				print ("Returned " + items[i]);
+				return items[i];
+			}
+		}
+		return null;
+	}
+	public Item returnItemByName(string name)
+	{
+		for (int i= 0; i<items.Count; i++)
+		{
+			if (name == items[i].itemName)
+			{
+				return items[i];
+			}
+		}
+		return null;
+	}
+	
+}
